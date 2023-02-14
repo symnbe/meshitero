f23f7fc26ceba51594de60b45011fcc6bcb49b89
symnbe <ookaminingen@me.com>
2023-02-14 07:24:51 +0000

    [add]いいね機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,7 +9,7 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
-  
+
   def index
     @post_images = PostImage.all
   end
@@ -17,7 +17,7 @@ class PostImagesController < ApplicationController
   def show
     @post_image = PostImage.find(params[:id])
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
-  root "homes#top"
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :post_images, only: [:new, :create, :index, :show, :destroy]
+   devise_for :users
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 
-  get "/homes/about" => "homes#about",as:'about'
 end
